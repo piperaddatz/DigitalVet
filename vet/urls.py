077888d4b5d82922cda71b3403894165b698b364
Pipe Raddatz <raddatz.felipe@gmail.com>
2021-03-28 00:48:12 -0300

    admin menu clinica y medicos

--- vet/urls.py
+++ vet/urls.py
@@ -5,7 +5,9 @@ app_name='vet'
 
 urlpatterns = [
         path(r'', views.index, name="index"),
-        path('mascotas/listado', views.mascotasListado, name="mascotas"),
+        path('mascotas/listado', views.mascotasListado, name="mascotas-list"),
+        path('clinica/listado', views.clinicaListado, name="clinica-list"),
+        path('medicos/listado', views.medicosListado, name="medicos-list"),
         path('accounts/profile/', views.PerfilUsuario, name='perfil'),
         path('accounts/salir/', views.salir, name='salir'),
 ]
--- vet/views.py
+++ vet/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from . import forms
 from .forms import CustomUserCreationForm
-from .models import CustomUser, Mascota
+from .models import CustomUser, Mascota, Clinica
+from django.contrib.auth.models import User
 from django.contrib.auth import logout
 
 # Create your views here.
@@ -17,6 +18,29 @@ def mascotasListado(request):
     print("USUARIO:", request.user.rol)
 
     return render(request, 'mascotas/listado.html', {"mascotas":mascotas})
+
+
+def clinicaListado(request):
+    clinicas = Clinica.objects.all()
+    if not request.user.is_authenticated:
+        return redirect('/accounts/login/')
+
+    print("USUARIO:", request.user.rol)
+
+    return render(request, 'clinicas/listado.html', {"clinicas":clinicas})
+
+
+
+def medicosListado(request):
+    users = CustomUser.objects.filter(rol="medico")
+    if not request.user.is_authenticated:
+        return redirect('/accounts/login/')
+
+    print("USUARIO:", request.user.rol)
+
+    return render(request, 'medicos/listado.html', {"users":users})
+
+
 
 def PerfilUsuario(request):
     return render(request, 'registration/profile.html', {})
